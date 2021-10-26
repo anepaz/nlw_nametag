bfda5d82e3cc000ba0a56574794cbf0ed5d62ff4
anepaz <anelisespaz@gmail.com>
2021-10-26 16:35:13 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const linksSocialMedia = {
+  github: 'anepaz',
+  youtube: 'UC5-Z5XP1Qt8q8MjRckyTnJA',
+  facebook: 'anelisepaz',
+  instagram: 'aneepsza_',
+  twitter: 'anniegoesmia'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      UserImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
